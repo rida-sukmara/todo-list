3632445d628e8ef8c6c59c5ba814350d2adefe1c
ridasukmara <ridasukmara@hotmail.com>
2022-01-10 17:17:02 +0700

    implement tomap mixins on category and task models

--- lib/data/models/category.dart
+++ lib/data/models/category.dart
@@ -1,10 +1,20 @@
 import 'package:equatable/equatable.dart';
+import 'package:todo_list/core/utils/to_mapper_mixins.dart';
 
-class Category extends Equatable {
+class Category extends Equatable with ToMapper {
+  static const tableName = 'tbl_category';
   final String name;
+  final int id;
 
-  const Category({required this.name});
+  const Category({required this.name, this.id = 0});
 
   @override
   List<Object?> get props => [name];
+
+  @override
+  Map<String, dynamic> toMap() {
+    return {
+      'name': name
+    };
+  }
 }
--- lib/data/models/task.dart
+++ lib/data/models/task.dart
@@ -1,18 +1,33 @@
 import 'package:equatable/equatable.dart';
+import 'package:todo_list/core/utils/to_mapper_mixins.dart';
 import 'package:todo_list/data/models/category.dart';
 
-class Task extends Equatable {
+class Task extends Equatable with ToMapper {
+
+  static const tableName = 'tbl_task';
+
   final String name;
   final DateTime? dateTime;
   final Category category;
+  final bool complated;
 
   bool isTodayTask() {
     return dateTime?.day == DateTime.now().day;
   }
 
   const Task(
-      {required this.name, required this.category, this.dateTime });
+      {required this.name, required this.category, this.complated = false, this.dateTime });
 
   @override
   List<Object?> get props => [name, dateTime, category];
+
+  @override
+  Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+      'dateTime': dateTime.toString(),
+      'complated': complated ? 1 : 0,
+      'category_id': category.id,
+    };
+  }
 }
